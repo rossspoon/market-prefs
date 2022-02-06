d72370dd1409988cc160c69d5957fc6a16c27d4b
rossspoon <rspoon@vt.edu>
2022-02-06 02:09:18 -0500

    Finish up Instructions

--- instructions/models.py
+++ instructions/models.py
@@ -9,7 +9,6 @@ from otree.api import (
     currency_range,
 )
 
-
 doc = """
 Instructions for the Short-squeeze market experiment
 """
@@ -30,4 +29,58 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+    ## QUIZ 1
+    qz1q1 = models.IntegerField(choices=[1, 2, 3, 4, 5, 6, 7, 8],
+                                label='What is the maximum number of orders that you may place in each round?')
+    qz1q2 = models.BooleanField(label="It is possible to both buy and sell shares in a single market round.")
+    qz1q3 = models.BooleanField(label="When you short the STOCK you must pay out the dividend.")
+    qz1q4 = models.StringField(choices=['1%', '2%', '3%', '4%', '5%', '6%', '7%'],
+                               label='What is the interest rate paid out on CASH?')
+
+    @staticmethod
+    def qz1q1_error_message(player, value):
+        if value != 6:
+            return "You are allowed up to 6 orders per market period."
+
+    @staticmethod
+    def qz1q2_error_message(player, value):
+        if value:
+            return "You may not BUY and SELL at the same time."
+
+    @staticmethod
+    def qz1q3_error_message(player, value):
+        if not value:
+            return "When you short the STOCK, dividends  will be deducted from you CASH holdings."
+
+    @staticmethod
+    def qz1q4_error_message(player, value):
+        if value != '5%':
+            return "Interest on CASH is earned at 5%."
+
+    ## QUIZ 2
+    qz2q1 = models.CurrencyField(label="At what price will shares of STOCK be bought back at the end of the experiment?",
+                                 choices=[1000, 1100, 1200, 1300, 1400, 1500])
+    qz2q2 = models.IntegerField(label="An automatic BUY-IN will occur when:",
+                                widget=widgets.RadioSelect,
+                                choices=[[0, 'The amount of CASH you borrowed is more than 50% of the value of you STOCK holdings'],
+                                         [1, 'The value of you shorted shares of STOCK is more that 50% of your amount of CASH'],
+                                         [2, 'Every two rounds to ensure an equal distribution of STOCK']])
+    qz2q3 = models.IntegerField(label="How many market periods will you participate in today?",
+                                choices=[10, 20, 30, 40, 50, 60])
+
+    @staticmethod
+    def qz2q1_error_message(player, value):
+        if value != 1400:
+            return 'The system will buy back share of STOCK at a price of 1400 points'
+
+    @staticmethod
+    def qz2q2_error_message(player, value):
+        if value != 1:
+            return 'The value of you shorted shares of STOCK is more that 50% of your amount of CASH'
+
+    @staticmethod
+    def qz2q3_error_message(player, value):
+        if value != 50:
+            return 'The market will last for 50 periods'
+
+
--- instructions/pages.py
+++ instructions/pages.py
@@ -3,35 +3,102 @@ import sys
 
 from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
-from .models import Constants
+from .models import Constants, Player
+
 
 class IntroPage(Page):
     pass
 
+
 class InstructionPage_01(Page):
     pass
+
 
 class InstructionPage_02(Page):
     pass
 
+
 class InstructionPage_03(Page):
     pass
+
 
 class InstructionPage_04(Page):
     pass
 
+
 class InstructionPage_05(Page):
     pass
 
+
 class InstructionPage_06(Page):
     pass
+
 
 class InstructionPage_07(Page):
     pass
 
 
-def get_instruction_class_names():
-    return [obj for name, obj in inspect.getmembers(sys.modules[__name__], lambda x: inspect.isclass(x) and x.__name__.startswith('InstructionPage'))]
+class InstructionPage_08(Page):
+    pass
 
 
-page_sequence = [IntroPage] + get_instruction_class_names()
+class InstructionPage_09(Page):
+    pass
+
+
+class InstructionPage_10(Page):
+    pass
+
+
+class InstructionPage_11(Page):
+    pass
+
+
+class InstructionPage_12(Page):
+    pass
+
+
+class InstructionPage_13(Page):
+    pass
+
+
+class InstructionPage_14(Page):
+    pass
+
+
+class InstructionPage_15(Page):
+    pass
+
+
+class Quiz01(Page):
+    form_model = Player
+    form_fields = ['qz1q1', 'qz1q2', 'qz1q3', 'qz1q4']
+
+
+class Quiz02(Page):
+    form_model = Player
+    form_fields = ['qz2q1', 'qz2q2', 'qz2q3']
+
+class OutroPage(Page):
+    pass
+
+
+page_sequence = [IntroPage,
+                 InstructionPage_01,
+                 InstructionPage_02,
+                 InstructionPage_03,
+                 InstructionPage_04,
+                 InstructionPage_05,
+                 InstructionPage_06,
+                 InstructionPage_07,
+                 Quiz01,
+                 InstructionPage_08,
+                 InstructionPage_09,
+                 InstructionPage_10,
+                 InstructionPage_11,
+                 InstructionPage_12,
+                 InstructionPage_13,
+                 InstructionPage_14,
+                 InstructionPage_15,
+                 Quiz02,
+                 OutroPage]
