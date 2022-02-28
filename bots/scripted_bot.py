774e3bb9af409485fbef83b0b350b9112d64e9b2
rossspoon <rspoon@vt.edu>
2022-02-28 15:10:47 -0500

    create framework for testing bots. Now, I should be able to easily generate tests cases

--- bots/scripted_bot.py
+++ bots/scripted_bot.py
@@ -1,14 +1,13 @@
 import sys
 import unittest
-from threading import Lock
 
 from otree import database
+from otree.bots.bot import ExpectError
 
 from rounds import *
 
-P_TYPE_TREATMENT = 0
-P_TYPE_BUYER = 1
-P_TYPE_SELLER = 2
+BUY = -1
+SELL = 1
 
 
 def run_unit_tests():
@@ -21,167 +20,8 @@ def run_unit_tests():
 
     ttr = unittest.TextTestRunner(stream=sys.stdout)
     ttr.run(suite)
+    database.db.rollback()
     print("\n FINISHED UNIT TESTS\n===========================\n\n")
-
-
-class ScriptedBot(Bot):
-    p_type = -1
-
-    def __init__(self,
-                 case_number: int,
-                 app_name,
-                 player_pk: int,
-                 subsession_pk: int,
-                 session_pk,
-                 participant_code,
-                 ):
-        super().__init__(case_number, app_name, player_pk, subsession_pk, session_pk, participant_code)
-
-        #  Custom
-        self.p_type = (self.player.id_in_group - 1) % 3  # 0 - treatment  :: 1 - first buyer :: 2 - first seller
-
-    # only run the unit tests once, this lock of class-level variable control that
-    lock = Lock()
-    unit_tests_run = False
-
-    def init_tests(self):
-        self.session.vars['EXIT_ROUND'] = 5
-
-        """ Run Unit tests and first-round player tests """
-        # Only run the unit tests once
-        if not ScriptedBot.unit_tests_run:
-            self.lock.acquire()
-            if not ScriptedBot.unit_tests_run:
-                ScriptedBot.unit_tests_run = True
-                run_unit_tests()
-                database.db.rollback()
-            self.lock.release()
-
-        # test treatment assignments
-        cash_control = self.session.config['cash_endowment_control']
-        shares_control = self.session.config['shares_endowment_control']
-        cash_treatment = self.session.config['cash_endowment_treatment']
-        shares_treatment = self.session.config['shares_endowment_treatment']
-
-        print("PLAYER:", self.player.id_in_group, self.p_type, self.player.cash, self.player.shares)
-        if self.p_type == P_TYPE_TREATMENT:
-            # Should be treatment
-            expect(self.player.cash, cash_treatment)
-            expect(self.player.shares, shares_treatment)
-
-        else:
-            # Should be control
-            expect(self.player.cash, cash_control)
-            expect(self.player.shares, shares_control)
-
-    def before_market_page_tests(self):
-        r_num = self.round_number
-
-        # the float should never change
-        expect(self.group.float, 10)
-
-        if r_num == 1:
-            expect(self.group.short, 10)
-        elif r_num == 2:
-            expect(self.group.short, 10)
-        elif r_num == 3:
-            expect(self.group.short, 10)
-        elif r_num == 4:
-            expect(self.group.short, 5)
-        elif r_num == 5:
-            expect(self.group.short, 5)
-
-        # Player-level tests
-        if r_num == 2 and self.p_type == P_TYPE_BUYER:
-            expect(self.player.cash, 0)
-            expect(self.player.shares, 20)
-            expect(self.player.is_auto_buy(), False)
-
-        elif r_num == 2 and self.p_type == P_TYPE_SELLER:
-            expect(self.player.cash, 1000)
-            expect(self.player.shares, 0)
-            expect(self.player.is_auto_buy(), False)
-
-        elif r_num == 2 and self.p_type == P_TYPE_TREATMENT:
-            expect(self.player.cash, 1000)
-            expect(self.player.shares, -10)
-            expect(self.player.is_auto_buy(), True)
-
-        elif r_num == 4 and self.p_type == P_TYPE_TREATMENT:
-            expect(self.player.cash, 750)
-            expect(self.player.shares, -5)
-            expect(self.player.is_auto_buy(), False)
-
-        elif r_num == 4 and self.p_type == P_TYPE_BUYER:
-            expect(self.player.cash, 250)
-            expect(self.player.shares, 15)
-            expect(self.player.is_auto_buy(), False)
-
-    def after_market_page_tests(self):
-        r_num = self.round_number
-
-        # In this test, all dividends are 0
-        expect(self.group.dividend, 0)
-
-        # Round 1 tests
-        if r_num == 1:
-            expect(self.group.price, 50)
-            expect(self.group.volume, 10)
-
-        if r_num == 2:
-            expect(self.group.price, 50)
-            expect(self.group.volume, 0)
-
-        if r_num == 3:
-            expect(self.group.price, 50)
-            expect(self.group.volume, 5)
-
-        # Round 2 & 3 Tests for Treatment player
-        # immediately after the close of the round-2 market
-        # the treatment player should have submitted an auto buy-in order
-        if r_num in [2, 3] and self.p_type == P_TYPE_TREATMENT:
-            orders = Order.filter(player=self.player)
-            expect(len(orders), 1)
-            o = orders[0]
-            expect(o.order_type, -1)
-            expect(o.price, 55)
-            expect(o.quantity, 5)
-
-    def play_round(self):
-        if self.round_number > 5:
-            return None
-
-        pid = self.player.id_in_group
-        print("\n==================\nRound Number:", self.round_number, "  Player:", pid)
-
-        # overhead
-        if self.round_number == 1:
-            self.init_tests()
-
-        # Market Page Tests
-        self.before_market_page_tests()
-        yield Market
-        self.after_market_page_tests()
-
-
-# LIVE METHOD TESTS
-def test_place_order(method, id_, type_, price, quant, valid=True, code_expect=0):
-    res = method(id_, {'func': 'submit-order', 'data': {'type': type_, 'price': price, 'quantity': quant}})
-    data = res.get(id_)
-
-    if type(code_expect) is OrderErrorCode:
-        code_expect = code_expect.value
-
-    if valid:
-        expect(data.get('func'), 'order_confirmed')
-        expect(data.get('order_id'), '>', 0)
-    else:
-        code_actual = data.get('error_code')
-        expect(data.get('func'), 'order_rejected')
-
-        expect(code_actual, code_expect)
-
-    return res
 
 
 def test_get_orders_for_player(method, id_, expected_num):
@@ -195,10 +35,10 @@ def test_get_orders_for_player(method, id_, expected_num):
 
 
 def test_delete_order(method, player_id, oid):
-    res = method(player_id, {'func': 'delete_order', 'oid': oid})
+    method(player_id, {'func': 'delete_order', 'oid': oid})
 
 
-def round_1_live_tests(method, kwargs):
+def run_order_placement_coverage_tests(method):
     # First test errors codes for rejecting order problems
     # test order deletion
     # generate some sell orders for player 1 and delete them.
@@ -221,31 +61,300 @@ def round_1_live_tests(method, kwargs):
     # test_place_order(method, 2, '2', 'a', '-1', valid=False, code_expect=22)
     test_place_order(method, 2, '2', '-1', 'a', valid=False, code_expect=25)
 
-    #######
-    # Now create the actual orders used in round one.
-    # valid orders
-    test_place_order(method, 2, '-1', '50', '10')  # buy
-    test_get_orders_for_player(method, 2, 1)
 
-    test_place_order(method, 3, '1', '50', '10')   # sell
-    test_get_orders_for_player(method, 3, 1)
+#############################################################################################################
+class Actor:
+    def __init__(self, name: str):
+        self.player = None
+        self.rounds = {}
+        self.name = name
+
+    def round(self, round_number):
+        actor_round = ActorRound(round_number)
+        self.rounds[round_number] = actor_round
+        return actor_round
+
+    def for_round(self, round_number):
+        return self.rounds.get(round_number)
 
 
-def round_2_live_tests(method, kwargs):
-    pass
+class ActorRound:
+    def __init__(self, round_number):
+        self.round_number = round_number
+        self.orders = []
+        self.cash = None
+        self.shares = None
+        self.expect_values = {}
+
+    # Configure Interface
+    def set(self, cash, shares):
+        self.cash = cash
+        self.shares = shares
+        return self
+
+    def buy(self, shares, at=None):
+        self._store_order(BUY, shares, at)
+        return self
+
+    def sell(self, shares, at=None):
+        self._store_order(SELL, shares, at)
+        return self
+
+    def expect(self, **kwargs):
+        self.expect_values.update(kwargs)
+        return self
+
+    # Round Interface
+    def do_set(self):
+        return self.cash is not None and self.shares is not None
+
+    def expects_any(self):
+        return len(self.expect_values.keys()) > 0
+
+    def expects(self, key):
+        return key in self.expect_values
+
+    # Helpers
+    def _store_order(self, otype, shares, price):
+        self.orders.append(dict(otype=otype, shares=shares, price=price))
 
 
-def round_3_live_tests(method, kwargs):
-    test_place_order(method, 2, '1', '50', '10')
+class MarketTests:
+    def __init__(self):
+        self.rounds = {}  # round number to MarketRound
+        self.part_id_to_actor_name = {}  # participant id to actor name
+        self.name_to_player = {}
+        self.actor_names = set()  # set of actor names
+
+    # Bot Interface
+    def attach_actors(self, players):
+        for name, player in zip(self.actor_names, players):
+            part_id = player.participant.code
+            self.part_id_to_actor_name[part_id] = name
+            self.name_to_player[name] = player
+
+    def get_actor_name(self, player):
+        part_id = player.participant.code
+        name = self.part_id_to_actor_name.get(part_id)
+
+        return name
+
+    def for_round(self, round_number):
+        """
+        Gets the set of actor-actions for the given round number
+        @param round_number: The round number
+        @return: MarketRound The market round for the given round number
+        """
+        return self.rounds.get(round_number)
+
+    def for_player_and_round(self, player, round_number):
+        """
+        Get the player actions for the given player in the given round
+        @param round_number: the given round number
+        @param player: the given player
+        @return: ActorRound for the player in this round
+        """
+        mr: MarketRound = self.for_round(round_number)
+        if not mr:
+            return None
+
+        name = self.get_actor_name(player)
+        return mr.actions_for_actor(name)
+
+    def get_num_defined_rounds(self):
+        return len(self.rounds.keys())
+
+    # Config Interface
+    def round(self, round_number):
+        market_round = MarketRound(self, round_number)
+        self.rounds[round_number] = market_round
+        return market_round
+
+
+class MarketRound:
+    def __init__(self, tests: MarketTests, round_number):
+        self.tests = tests
+        self.round_number = round_number
+        self.player_actions = {}  # ActionRounds  - The player actions for a particular round
+        self.expect_values = {}
+
+    # Configuration Interface
+    def expect(self, **kwargs):
+        self.expect_values.update(kwargs)
+        return self
+
+    def actor(self, name: str, setup):
+        ar = ActorRound(self.round_number)
+        # tell the MarketTest object about this
+        self.tests.actor_names.add(name)
+        self.player_actions[name] = ar
+        setup(ar)
+        return self
+
+    def finish(self):
+        return self.tests
+
+    # Bot Interface
+    def actions_for_actor(self, name):
+        return self.player_actions.get(name)
+
+    def expects_any(self):
+        return len(self.expect_values.keys()) > 0
+
+    def expects(self, key):
+        return key in self.expect_values
+
+
+def test_object_attribute(obj, actions, attr):
+    if actions.expects(attr):
+        print(f"in round {actions.round_number}: testing: ", attr)
+        actual = obj.field_maybe_none(attr)
+        expected = actions.expect_values.get(attr)
+
+        try:
+            expect(actual, expected)
+        except ExpectError as err:
+            if type(obj) is Group:
+                raise ExpectError(f"In round {actions.round_number}: {err}")
+
+
+#############################################################################################################
+class ScriptedBot(Bot):
+    first_time = True
+    last_round_tested = 0
+
+    def play_round(self):
+        # The first time we get here, we need to assign players to all the actor objects
+        if self.first_time:
+            ScriptedBot.first_time = False
+            market.attach_actors(self.group.get_players())
+            run_unit_tests()
+
+        round_number = self.round_number
+        # exit if we are past the defined rounds
+        if round_number > market.get_num_defined_rounds() + 1:
+            return
+
+        first_time_in_round = False
+        if ScriptedBot.last_round_tested < round_number:
+            first_time_in_round = True
+            ScriptedBot.last_round_tested = round_number
+
+        if first_time_in_round:
+            print(f"===========\n\tROUND {round_number}")
+
+        player = self.player
+        actor_name = market.get_actor_name(player)
+        print(f"Player: {actor_name}")
+        this_round: ActorRound = market.for_player_and_round(player, round_number)
+        last_round: ActorRound = market.for_player_and_round(player, round_number - 1)
+
+        # last round tests are usually to test market results
+        if last_round:
+            self.last_round_tests(last_round)
+
+        # market level tests
+        last_round_market: MarketRound = market.for_round(round_number - 1)
+        if last_round_market and first_time_in_round:
+            self.last_round_market_tests(last_round_market)
+
+        # Set up Player for round
+        if this_round and this_round.do_set():
+            player.cash = this_round.cash
+            player.shares = this_round.shares
+
+        yield Market
+        if this_round:
+            self.after_market_page_tests(actor_name, this_round)
+
+    def last_round_tests(self, actions):
+        if not actions.expects_any():
+            return
+
+        player = self.player
+        for var in vars(player):
+            test_object_attribute(player, actions, var)
+
+    def after_market_page_tests(self, actor, actions):
+        pass
+
+    def init_tests(self):
+        pass
+
+    def last_round_market_tests(self, last_round_market: MarketRound):
+        if not last_round_market.expects_any():
+            return
+
+        if self.round_number > 1:
+            group = self.group.in_round(self.round_number - 1)
+            for var in vars(group):
+                test_object_attribute(group, last_round_market, var)
+
+
+# LIVE METHOD TESTS
+def test_place_order(method, id_, type_, price, quant, valid=True, code_expect=0):
+    _price = str(price)
+    _quant = str(quant)
+    _type = str(type_)
+    res = method(id_, {'func': 'submit-order', 'data': {'type': _type, 'price': _price, 'quantity': _quant}})
+    data = res.get(id_)
+
+    if type(code_expect) is OrderErrorCode:
+        code_expect = code_expect.value
+
+    if valid:
+        expect(data.get('func'), 'order_confirmed')
+        expect(data.get('order_id'), '>', 0)
+    else:
+        code_actual = data.get('error_code')
+        expect(data.get('func'), 'order_rejected')
+
+        expect(code_actual, code_expect)
+
+    return res
 
 
 def call_live_method(method, **kwargs):
-    r_num = kwargs.get('round_number')
-    if r_num == 1:
-        round_1_live_tests(method, kwargs)
+    round_number = kwargs.get('round_number')
+    group: Group = kwargs.get('group')
 
-    elif r_num == 2:
-        round_2_live_tests(method, kwargs)
+    if round_number > market.get_num_defined_rounds():
+        return
 
-    elif r_num == 3:
-        round_3_live_tests(method, kwargs)
+    # Run coverage tests in the first round
+    if round_number == 1:
+        run_order_placement_coverage_tests(method)
+
+    for player in group.get_players():
+        ar: ActorRound = market.for_player_and_round(player, round_number)
+        for o in ar.orders:
+            test_place_order(method, player.id_in_group, o['otype'], o['price'], o['shares'])
+
+
+#############################################################################################################
+market = MarketTests().round(1) \
+    .expect(price=500, volume=1) \
+        .actor("Buyer", lambda ar: ar.set(1000, 5)
+               .buy(1, at=500)
+               .expect(cash=500, shares=6, periods_until_auto_buy=-99)) \
+        .actor("Seller", lambda ar: ar.set(1000, 5)
+               .sell(1, at=500)
+               .expect(cash=1500, shares=4, periods_until_auto_buy=-99)) \
+        .actor("Treated", lambda ar: ar.set(2000, -2)
+               .expect(cash=2000, shares=-2, periods_until_auto_buy=0)) \
+        .finish() \
+    .round(2) \
+        .expect(price=500, volume=0) \
+        .actor("Buyer", lambda ar: ar.expect(cash=500, shares=6, periods_until_auto_buy=-99)) \
+        .actor("Seller", lambda ar: ar.expect(cash=1500, shares=4, periods_until_auto_buy=-99,
+                                              interest_earned=0, dividend_earned=0)) \
+        .actor("Treated", lambda ar: ar.expect(cash=2000, shares=-2, periods_until_auto_buy=0)) \
+        .finish() \
+    .round(3) \
+        .expect(price=550, volume=1) \
+        .actor("Buyer", lambda ar: ar.expect(cash=500, shares=6, periods_until_auto_buy=-99)) \
+        .actor("Seller", lambda ar: ar.sell(1, at=500)
+               .expect(cash=2050, shares=3, periods_until_auto_buy=-99,
+                       interest_earned=0, dividend_earned=0)) \
+        .actor("Treated", lambda ar: ar.expect(cash=1450, shares=-1, periods_until_auto_buy=None)) \
+        .finish()
