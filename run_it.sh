040c0c5bdf86afcaa1b53959e36e13d195c8a0dc
rossspoon <rspoon@vt.edu>
2022-03-13 15:00:11 -0400

    First crack at setting up a room, and getting production ready.   run_it.sh script to get everything going.

---
+++ run_it.sh
@@ -0,0 +1,14 @@
+export OTREE_ADMIN_PASSWORD=barry
+export OTREE_PRODUCTION=1 # uncomment this line to enable production mode
+#export OTREE_AUTH_LEVEL=DEMO
+export DATABASE_URL=postgres://postgres@localhost/squeeze
+
+otree resetdb
+otree prodserver 192.168.1.135:8000
+#brew services start postgresql
+#usr/local/opt/postgres/bin/createuser -s postgres
+#brew services stop postgresql
+
+unset DATABASE_URL
+
+
