110843e07665b04d9d88849b0d0b8c1adc6051ce
rossspoon <rspoon@vt.edu>
2022-04-05 10:17:28 -0400

    Change this back to work on my machine.  Heroku handles db access automatically.

--- run_it.sh
+++ run_it.sh
@@ -1,10 +1,10 @@
 export OTREE_ADMIN_PASSWORD=b9rry@
 export OTREE_PRODUCTION=1 # uncomment this line to enable production mode
-#export OTREE_AUTH_LEVEL=DEMO
-export DATABASE_URL=postgres://postgres@localhost/DATABASE
+export OTREE_AUTH_LEVEL=STUDY
+export DATABASE_URL=postgres://postgres@localhost/squeeze
 
-otree resetdb
-otree prodserver 192.168.1.135:8000
+#otree resetdb
+otree prodserver 8000
 #brew services start postgresql
 #usr/local/opt/postgres/bin/createuser -s postgres
 #brew services stop postgresql
