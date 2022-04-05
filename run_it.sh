08219e88ce32ce948c7d7298a9844660ad6dff30
rossspoon <rspoon@vt.edu>
2022-04-05 09:13:03 -0400

    DataBase name

--- run_it.sh
+++ run_it.sh
@@ -1,7 +1,7 @@
 export OTREE_ADMIN_PASSWORD=b9rry@
 export OTREE_PRODUCTION=1 # uncomment this line to enable production mode
 #export OTREE_AUTH_LEVEL=DEMO
-export DATABASE_URL=postgres://postgres@localhost/squeeze
+export DATABASE_URL=postgres://postgres@localhost/DATABASE
 
 otree resetdb
 otree prodserver 192.168.1.135:8000
