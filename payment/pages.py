15e89e8697a31bf20674d7f83caa988c7931e1e1
rossspoon <rspoon@vt.edu>
2022-06-09 17:33:54 -0400

    Add session completion link.

--- payment/pages.py
+++ payment/pages.py
@@ -7,6 +7,15 @@ class ConsentDeniedPage(Page):
         participant = self.player.participant
         return not participant.vars.get('CONSENT') and participant.vars.get('CONSENT_BUTTON_CLICKED')
 
+    def vars_for_template(self):
+        is_online = scf.is_online(self.player)
+        url = self.player.session.vars.get('prolific_completion_url')
+        if not url:
+            url = "https://www.vt.edu"
+
+        return {'is_online': is_online,
+                'prolific_completion_url': url}
+
 
 class FinalResultsPage(Page):
     def vars_for_template(self):
@@ -15,11 +24,16 @@ class FinalResultsPage(Page):
         market_bonus = participant.vars.get('MARKET_PAYMENT').to_real_world_currency(session)
         forecast_bonus = participant.vars.get('FORECAST_PAYMENT').to_real_world_currency(session)
         is_online = scf.is_online(self.player)
+        url = self.player.session.vars.get('prolific_completion_url')
+        if not url:
+            url = "https://www.vt.edu"
+
         return {'market_bonus': market_bonus,
                 'forecast_bonus': forecast_bonus,
                 'total_bonus': market_bonus + forecast_bonus,
                 'total_pay': participant.payoff_plus_participation_fee(),
-                'is_online': is_online}
+                'is_online': is_online,
+                'prolific_completion_url': url}
 
     def is_displayed(self):
         participant = self.player.participant
