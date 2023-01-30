55c2f6ed6d87273eabd435d96ddce8af79265784
rossspoon <rspoon@vt.edu>
2023-01-30 15:17:43 -0500

    Readding the windows start file

---
+++ run_it.bat
@@ -0,0 +1,16 @@
+set OTREE_ADMIN_PASSWORD=b9rry@
+set OTREE_AUTH_LEVEL=1
+
+REM Comment out this line to turn on debug mode
+set OTREE_PRODUCTION=1
+
+REM Change this to control access
+set OTREE_AUTH_LEVEL=STUDY
+REM set OTREE_AUTH_LEVEL=DEMO
+
+
+set DATABASE_URL=postgres://vteconlab:econvt!@localhost/squeeze
+
+
+REM otree resetdb
+otree prodserver econlabhost18s:80
