da5273c67411f82b7197a34b174d4bb192d64370
rossspoon <rspoon@vt.edu>
2023-12-04 15:43:56 -0500

    Faster DOSE calculations.  special script to read in the dec tree json and generate a human-readable file.

--- common/BinTree.py
+++ common/BinTree.py
@@ -19,3 +19,26 @@ class Node:
         #go left
         if self.left:
             self.left.print_tree(level=next_lev)
+    
+    
+    @staticmethod
+    def str_helper(node, level=0):
+        str = ""
+        
+        next_lev = level+1
+        # go right
+        if node.right:
+            str += node.str_helper(node.right, level=next_lev)
+
+        # visit this node
+        pad = '\t' * level;
+        str += f"{pad} {float(node.val) :.2f} \n"
+
+        #go left
+        if node.left:
+            str += node.str_helper(node.left, level=next_lev)
+            
+        return str
+
+    def __str__(self):
+        return self.str_helper(self)
