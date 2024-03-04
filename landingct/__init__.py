fb5ae411645d9e57b7597829f1076c2db3d51005
rossspoon <rspoon@vt.edu>
2024-03-04 15:15:30 -0500

    revamp quiz page.  record initial entries, allow multiple submission.  next button appears when the quiz is completely correct

--- landingct/__init__.py
+++ landingct/__init__.py
@@ -40,34 +40,41 @@ class Player(BasePlayer):
     timeout = models.FloatField(initial=0.0)
     consent_given = models.BooleanField(blank=True)
     
-    quiz_1 = models.IntegerField(blank=True,
+    quiz_1 = models.StringField(blank=True,
                 label="During order entry you want to input a BUY order at price of 8.52 and a SELL order at a price of 7.89. What will happen to your orders for that period?",
-                choices=[[1, "They'll be entered as usual"],
-                         [2, "The SELL order will not go through"]]
+                choices=[["1", "They'll be entered as usual"],
+                         ["2", "The SELL order will not go through"]]
                 )
-    quiz_2 = models.IntegerField(blank=True,
+    quiz_2 = models.StringField(blank=True,
                 label="During order entry the highest price at which you submit a BUY order is 16.78 and the lowest price at which others SELL is 16.22. The market price is 16.56 for that period. You will:",
-                choices=[[1, "BUY one unit at 16.78"],
-                         [2, "BUY one unit at 16.56"],
-                         [3, "SELL one unit at 16.56"],
-                         [4, "SELL one unit at 17.22"],
-                         [5, "Not Trade"],
+                choices=[["1", "BUY one unit at 16.78"],
+                         ["2", "BUY one unit at 16.56"],
+                         ["3", "SELL one unit at 16.56"],
+                         ["4", "SELL one unit at 17.22"],
+                         ["5", "Not Trade"],
                     ]
                 )
-    quiz_3 = models.IntegerField(blank=True,
+    quiz_3 = models.StringField(blank=True,
                 label="You have 200 units of CASH at the start of a trading period and no STOCK. You do not trade that period. How much CASH do you have at the beginning of the next period?",
-                choices=[200, 210, 205, 190]
+                choices=["200", "210", "205", "190"]
                 )
 
-    quiz_4 = models.IntegerField(blank=True,
+    quiz_4 = models.StringField(blank=True,
                 label = "Your account has 5 STOCK and 100 CASH at the start of a trading period, and you do not BUY or SELL during that period. The dividend for that round is 1.00. How much CASH do you have at the start of the next round?",
-                choices = [105, 110, 100, 114]
+                choices = ["105", "110", "100", "114"]
                 )
     
-    quiz_5 = models.IntegerField(blank=True,
+    quiz_5 = models.StringField(blank=True,
                  label = "After the final trading period, you have 4 remaining units of STOCK. The market price in the final period is 29. How many units of experiment CASH do you receive in exchange for your STOCK?",
-                 choices=[4, 29, 56, 116]
+                 choices=["4", "29", "56", "116"]
                 )
+    
+    quiz_1_init = models.StringField(blank=True)
+    quiz_2_init = models.StringField(blank=True)
+    quiz_3_init = models.StringField(blank=True)
+    quiz_4_init = models.StringField(blank=True)
+    quiz_5_init = models.StringField(blank=True)
+    
 
 
 def get_exp_link(player: Player):
@@ -92,7 +99,6 @@ class Consent(Page):
      @staticmethod 
      def vars_for_template(player: Player):
          config = player.session.config
-         print(config)
          return config
     
      
@@ -147,37 +153,98 @@ class Instructions(Page):
         config = player.session.config
         return config
     
+
+def quiz_grade_vars(player: Player, data:dict):
+        ## Grade the quiz
+        q1_score = 1 if data.get('quiz_1') == '2' else 0
+        q2_score = 1 if data.get('quiz_2') == '2' else 0
+        q3_score = 1 if data.get('quiz_3') == '210' else 0
+        q4_score = 1 if data.get('quiz_4') == '110' else 0
+        q5_score = 1 if data.get('quiz_5') == '56' else 0
+        total_score = q1_score + q2_score + q3_score + q4_score + q5_score
+        
+        return {
+                    1: q1_score,
+                    2: q2_score,
+                    3: q3_score,
+                    4: q4_score,
+                    5: q5_score,
+                    'total_score': total_score,
+                    }   
     
+def quiz_live_method(player, data):
+    func = data['func']
+    
+
+  
+    ###
+    ##
+    ## Gade the quiz
+    ##
+    ###
+    if func == 'grade_it':
+    
+        
+        form_data = data['data']
+        
+        # record initial responses
+        q1_i = player.field_maybe_none('quiz_1_init')
+        q2_i = player.field_maybe_none('quiz_2_init')
+        q3_i = player.field_maybe_none('quiz_3_init')
+        q4_i = player.field_maybe_none('quiz_4_init')
+        q5_i = player.field_maybe_none('quiz_5_init')
+        
+        if not q1_i and not q2_i and not q3_i and not q4_i and not q5_i:
+            player.quiz_1_init = form_data['quiz_1']
+            player.quiz_2_init = form_data['quiz_2']
+            player.quiz_3_init = form_data['quiz_3']
+            player.quiz_4_init = form_data['quiz_4']
+            player.quiz_5_init = form_data['quiz_5']
+            
+        
+            
+        grades = quiz_grade_vars(player, form_data)
+        show_next = grades['total_score'] == 5
+        ret = {player.id_in_group: dict(func = "graded",
+                                         grades = grades,
+                                         show_next = show_next
+                                         )}
+        
+        # Save form data to the player object for persistance
+        player.quiz_1 = form_data['quiz_1']
+        player.quiz_2 = form_data['quiz_2']
+        player.quiz_3 = form_data['quiz_3']
+        player.quiz_4 = form_data['quiz_4']
+        player.quiz_5 = form_data['quiz_5']
+        
+        
+        
+        return ret
+    
+    ###
+    ##
+    ## Initial Load - This persists current form entries across page loads
+    ##
+    ###
+    elif func == 'load':
+        ## Initial page load
+        ret = dict(func="load",
+                   id_quiz_1 = player.field_maybe_none('quiz_1'),
+                   id_quiz_2 = player.field_maybe_none('quiz_2'),
+                   id_quiz_3 = player.field_maybe_none('quiz_3'),
+                   id_quiz_4 = player.field_maybe_none('quiz_4'),
+                   id_quiz_5 = player.field_maybe_none('quiz_5'),
+                   )
+        return {player.id_in_group: ret}
+ 
+   
 class Quiz(Page):
     form_model = 'player'
     form_fields = ['quiz_1', 'quiz_2', 'quiz_3', 'quiz_4', 'quiz_5']
     # timeout_seconds = 600
 
-
-def quiz_grade_vars(player: Player):
-        ## Grade the quiz
-        q1_score = 1 if player.field_maybe_none('quiz_1') == 2 else 0
-        q2_score = 1 if player.field_maybe_none('quiz_2') == 2 else 0
-        q3_score = 1 if player.field_maybe_none('quiz_3') == 210 else 0
-        q4_score = 1 if player.field_maybe_none('quiz_4') == 110 else 0
-        q5_score = 1 if player.field_maybe_none('quiz_5') == 56 else 0
-        total_score = q1_score + q2_score + q3_score + q4_score + q5_score
-        
-        return dict(
-                    q={1: q1_score,
-                       2: q2_score,
-                       3: q3_score,
-                       4: q4_score,
-                       5: q5_score},
-                    total_score = total_score,
-                    )
-class QuizGrade(Page):
-    form_model = 'player'
-    form_fields = ['quiz_1', 'quiz_2', 'quiz_3', 'quiz_4', 'quiz_5']
-    # timeout_seconds=120
+    live_method = quiz_live_method
     
-    vars_for_template = quiz_grade_vars
-    js_vars = quiz_grade_vars
 
 
     
@@ -253,4 +320,4 @@ class ReadyToStart(Page):
     #     if player.session.enrolled_ids >= {{C.MIN_PLAYERS_PER_GROUP}}:
     #         return upcoming_apps[-1]
 
-page_sequence = [Consent, NoConsent,  Instructions, Quiz, QuizGrade, Survey1,  WaitForPlayers, ReadyToStart]
+page_sequence = [Consent, NoConsent,  Instructions, Quiz, Survey1,  WaitForPlayers, ReadyToStart]
