dfe69ded2afb92f4d30af60da8e519abbe4df3e2
rossspoon <rspoon@vt.edu>
2024-03-13 11:06:25 -0400

    helper script to make use of REST api

---
+++ bin/make_participant_finished.py
@@ -0,0 +1,64 @@
+#!python
+
+import requests  # pip3 install requests
+from pprint import pprint
+from os import environ
+import sys
+import getopt
+
+GET = requests.get
+POST = requests.post
+
+# if using Heroku, change this to https://YOURAPP.herokuapp.com
+LOCAL_SERVER_URL = 'http://localhost:8000'
+SERVER_URL = 'https://vt-market-experiment.herokuapp.com'
+BASE_URL = [SERVER_URL]
+REST_KEY = 's+Uj_g5Zunap3?TfZ6uUQ7'
+
+def call_api(method, *path_parts, **params) -> dict:
+    print(path_parts)
+    path_parts = '/'.join(path_parts)
+    url = f'{BASE_URL[0]}/api/{path_parts}/'
+    resp = method(url, json=params, headers={'otree-rest-key': REST_KEY})
+    if not resp.ok:
+        msg = (
+            f'Request to "{url}" failed '
+            f'with status code {resp.status_code}: {resp.text}'
+        )
+        raise Exception(msg)
+    return resp.json()
+
+
+IDS = ['7yie36v5',
+'n7xvxqf6',
+'37fxkf6n',
+'tua9l5ic',
+'3z5ajv7g',
+'a053vavw',
+'5eiyjet7',
+'xixbk2wi',
+'6w7zusl6',
+'aq32twsx',
+'3fjd4p07',
+'grvtqx2k',
+'q3eed83d',
+'5t2lsf81',
+'kr5aqbg4',
+'2q5u7nzm',
+'6mcv9x1g',
+'9rew9efn',
+]
+
+def main(argv):
+    
+    
+    sess_data = call_api(GET, 'session', '8h0lckuw')
+    sess_data['participants']
+    
+    # for id in IDS:
+    #     call_api(POST, 'participant_vars', id, vars=dict(finished=True))
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
